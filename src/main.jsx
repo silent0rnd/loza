fd26447808101a6e3d13065da56b1f5653e36595
Maxim <silent0rnd@gmail.com>
2026-07-16 16:47:39 +0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '@fontsource/manrope/400.css';
+import '@fontsource/manrope/500.css';
+import '@fontsource/manrope/700.css';
+import '@fontsource/unbounded/500.css';
+import '@fontsource/unbounded/700.css';
+import './styles.css';
+import App from './App.jsx';
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
